1dc2bf97734dd000122145da1fc4522ffb20adb3
houchuyi <51420183+houchuyi@users.noreply.github.com>
2023-12-03 16:47:56 -0500

    Upload Unity Platformer Demo

---
+++ Platformer/Assets/2D Platformer/Scripts/PlayerController.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platformer
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public float movingSpeed;
+        public float jumpForce;
+        private float moveInput;
+
+        private bool facingRight = false;
+        [HideInInspector]
+        public bool deathState = false;
+
+        private bool isGrounded;
+        public Transform groundCheck;
+
+        private Rigidbody2D rigidbody;
+        private Animator animator;
+        private GameManager gameManager;
+
+        private bool wasGrounded;
+
+        void Start()
+        {
+            rigidbody = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
+            gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+            wasGrounded = isGrounded;
+        }
+
+        private void FixedUpdate()
+        {
+            CheckGround();
+        }
+
+        void Update()
+        {
+            if (Input.GetButton("Horizontal")) 
+            {
+                moveInput = Input.GetAxis("Horizontal");
+                Vector3 direction = transform.right * moveInput;
+                transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, movingSpeed * Time.deltaTime);
+                animator.SetInteger("playerState", 1); // Turn on run animation
+            }
+            else
+            {
+                if (isGrounded) animator.SetInteger("playerState", 0); // Turn on idle animation
+            }
+            if(Input.GetKeyDown(KeyCode.Space) && isGrounded )
+            {
+                rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+            }
+            if (!isGrounded)animator.SetInteger("playerState", 2); // Turn on jump animation
+
+            if (!wasGrounded && isGrounded)
+            {
+                animator.SetInteger("playerState", 3); // Set to landing animation state
+                                                       // Optionally, add a delay or use an animation event to transition to idle or running animation
+                Debug.Log("landed");
+            }
+
+            // Update wasGrounded for the next frame
+            wasGrounded = isGrounded;
+
+            if (facingRight == false && moveInput > 0)
+            {
+                Flip();
+            }
+            else if(facingRight == true && moveInput < 0)
+            {
+                Flip();
+            }
+        }
+
+        private void Flip()
+        {
+            facingRight = !facingRight;
+            Vector3 Scaler = transform.localScale;
+            Scaler.x *= -1;
+            transform.localScale = Scaler;
+        }
+
+        private void CheckGround()
+        {
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.transform.position, 0.2f);
+            isGrounded = colliders.Length > 1;
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.tag == "Enemy")
+            {
+                deathState = true; // Say to GameManager that player is dead
+            }
+            else
+            {
+                deathState = false;
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.tag == "Coin")
+            {
+                gameManager.coinsCounter += 1;
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
